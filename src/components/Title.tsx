91e3543b5312000d146a03d46f53c8b74c332471
Ayuu0721 <ayuu.kumakuma@gmail.com>
2022-07-07 15:51:07 +0900

    Recoil導入

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <Box display={'flex'} justifyContent={'center'} pt={'4'}>
       <Text fontSize={'4xl'} pos={'absolute'}>
-        Ayuu's ToDo List
+        Ayuu's ToDo List!
       </Text>
     </Box>
   )
